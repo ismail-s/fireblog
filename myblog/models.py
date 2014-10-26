3ac880dd117f95b4aa870294f7ae3d65a3218791
Ismail S <ismail-s.no-reply@github.com>
2014-10-26 15:53:38 +0000

    Start using alembic to manage the db

--- myblog/models.py
+++ myblog/models.py
@@ -23,15 +23,15 @@ Base = declarative_base()
 
 class Post(Base):
     __tablename__ = 'posts'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, unique = True, index = True)
-    created = Column(DateTime, default=datetime.datetime.utcnow, index = True)
+    id = Column(Integer, primary_key=True, nullable = False)
+    name = Column(Text, unique = True, index = True, nullable = False)
+    created = Column(DateTime, default=datetime.datetime.utcnow, index = True, nullable = False)
     markdown = Column(Text)
     html = Column(Integer)
 
 class Users(Base):
     __tablename__ = 'users'
     # Note userid will be an email address from mozilla persona
-    id = Column(Integer, primary_key = True)
-    userid = Column(Text, unique = True, index = True)
+    id = Column(Integer, primary_key = True, nullable = False)
+    userid = Column(Text, unique = True, index = True, nullable = False)
     group = Column(Text)
