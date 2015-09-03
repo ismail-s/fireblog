80cbd93be55a337290bd36086b811aba13e8c809
Ismail S <ismail-s.no-reply@github.com>
2015-09-03 14:47:25 +0100

    More pep8 compliance changes

--- setup.py
+++ setup.py
@@ -28,31 +28,32 @@ requires = [
     'PyRSS2Gen',
     'alembic',
     'requests'
-    ]
+]
 
-setup(name='myblog',
-      version='0.0',
-      description='myblog',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='myblog',
-      install_requires=requires,
-      entry_points="""\
+if __name__ == '__main__':
+    setup(name='myblog',
+          version='0.0',
+          description='myblog',
+          long_description=README + '\n\n' + CHANGES,
+          classifiers=[
+              "Programming Language :: Python",
+              "Framework :: Pyramid",
+              "Topic :: Internet :: WWW/HTTP",
+              "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          ],
+          author='',
+          author_email='',
+          url='',
+          keywords='web wsgi bfg pylons pyramid',
+          packages=find_packages(),
+          include_package_data=True,
+          zip_safe=False,
+          test_suite='myblog',
+          install_requires=requires,
+          entry_points="""\
       [paste.app_factory]
       main = myblog:main
       [console_scripts]
       initialize_myblog_db = myblog.scripts.initializedb:main
       """,
-      )
+          )
