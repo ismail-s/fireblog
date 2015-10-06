d9d31986dd8ba24f8363cb2eb3701ea65888d43c
Ismail <ismail-s@users.noreply.github.com>
2015-10-06 14:17:05 +0100

    Switch RenderingPost import to events file

--- fireblog/tests/comments_test.py
+++ fireblog/tests/comments_test.py
@@ -1,5 +1,6 @@
 import fireblog.comments
 import fireblog.utils
+from fireblog.events import RenderingPost
 from fireblog.models import DBSession, Post
 import datetime
 
@@ -15,7 +16,7 @@ class Test_comment_view:
     @staticmethod
     def get_comment_list(postname, pyramid_req):
         post = DBSession.query(Post).filter_by(name=postname).first()
-        event = fireblog.utils.RenderingPost(post, pyramid_req)
+        event = RenderingPost(post, pyramid_req)
         return fireblog.comments.render_comments_list_from_event(event)
 
     def test_success(self, pyramid_config, pyramid_req):
