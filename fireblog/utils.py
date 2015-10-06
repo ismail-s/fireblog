8166ade85cfbec4c6e4de5f39ae1ff3ef79427fa
Ismail <ismail-s@users.noreply.github.com>
2015-10-06 13:26:52 +0100

    Reflow comment (pep8 conformance)

--- fireblog/utils.py
+++ fireblog/utils.py
@@ -14,13 +14,14 @@ from pyramid.httpexceptions import HTTPException
 try:
     region = get_region('')
 except KeyError:
-    # KeyError occurs when this module is directly imported before fireblog:main
-    # is called. Basically, in the fireblog:main function, pyramid_dogpile_cache
-    # plugin sets up dogpile.cache using settings from the usual ini files.
-    # However, if this module is imported first, then this setup doesn't
-    # happen. As a result, as a fallback in these cases (atm only when tests
-    # are run, which is when we actually want to check that the cache is
-    # correctly managed by the website) we use the memory cache backend.
+    # KeyError occurs when this module is directly imported before
+    # fireblog:main is called. Basically, in the fireblog:main function,
+    # pyramid_dogpile_cache plugin sets up dogpile.cache using settings
+    # from the usual ini files. However, if this module is imported first,
+    # then this setup doesn't happen. As a result, as a fallback in these
+    # cases (atm only when tests are run, which is when we actually want
+    # to check that the cache is correctly managed by the website) we use
+    # the memory cache backend.
     region = get_region('', backend='dogpile.cache.memory')
 
 
