147afff3e851fee2d375d1782a5cd60cf40c4efa
Ismail <astroman3d@gmail.com>
2015-10-21 13:50:20 +0000

    Add docstrings for events

--- fireblog/events.py
+++ fireblog/events.py
@@ -6,7 +6,7 @@ notified when the event is fired.
 
 class RenderingPost(object):
     """This is an event that gets fired when a post is being viewed.
-    Subscribers can add html sections to self.sections and these will be
+    Subscribers can add html sections to ``self.sections`` and these will be
     put below the post on the webpage.
     """
 
@@ -23,14 +23,17 @@ class _ModifyPost(object):
 
 
 class PostCreated(_ModifyPost):
+    "Event that is fired when a post is created."
     pass
 
 
 class PostDeleted(_ModifyPost):
+    "Event that is fired when a post is being deleted."
     pass
 
 
 class PostEdited(_ModifyPost):
+    "Event that is fired when a post is edited."
     pass
 
 
@@ -42,8 +45,10 @@ class _CommentModified(object):
 
 
 class CommentAdded(_CommentModified):
+    "Event that is fired when a comment has been added."
     pass
 
 
 class CommentDeleted(_CommentModified):
+    "Event that is fired when a comment has been deleted."
     pass
