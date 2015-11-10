c3bba432e8b6f6d576de6589d701d4ecaf2afe01
Ismail <astroman3d@gmail.com>
2015-11-10 16:47:10 +0000

    Add more tests

--- fireblog/tests/settings_test.py
+++ fireblog/tests/settings_test.py
@@ -1,11 +1,40 @@
+import pytest
 from fireblog.settings import settings
+
 
 def test_can_set_an_item_and_then_get_the_same_item(pyramid_config):
     assert not settings.get('test', None)
     settings['test'] = 'test_str'
     assert settings['test'] == 'test_str'
 
+
 def test_changing_an_item_and_getting_it_returns_new_item(pyramid_config):
     settings['test'] = 'test_str'
     settings['test'] = 'test_str1'
     assert settings['test'] == 'test_str1'
+
+
+def test_can_delete_an_item(pyramid_config):
+    settings['test'] = 'test_str'
+    assert settings['test']
+    del settings['test']
+    assert not settings.get('test', None)
+    with pytest.raises(KeyError):
+        settings['test']
+
+
+def test_can_get_num_of_settings(pyramid_config):
+    settings.update({'1': '1', '2': '2', '3': '3'})
+    assert len(settings) == 3
+    del settings['3']
+    assert len(settings) == 2
+    settings['test'] = 'te'
+    assert len(settings) == 3
+
+
+def test_can_iterate_over_keys(pyramid_config):
+    settings.update({'1': '1', '2': '2', '3': '3'})
+    results = set()
+    for key in settings:
+        results.add(key)
+    assert results == set(['1', '2', '3'])
