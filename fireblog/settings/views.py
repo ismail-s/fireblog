e10bc22443661a668a197f0d367df27ab813b66a
Ismail <astroman3d@gmail.com>
2015-11-12 17:35:37 +0000

    Move function into class

--- fireblog/settings/views.py
+++ fireblog/settings/views.py
@@ -1,10 +1,14 @@
-from fireblog.settings import mapping
+from fireblog.settings import mapping, registry_names
 from fireblog.utils import use_template, TemplateResponseDict
 from pyramid.view import view_config, view_defaults
 
 
-@view_config(route_name='settings', decorator=use_template(
-    'settings.mako'), permission=None, request_method="GET")
-def settings(request):
-    save_url = request.route_url('settings')
-    return TemplateResponseDict(mapping=mapping, save_url=save_url)
+@view_defaults(route_name='settings', permission=None)
+class Settings:
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(decorator=use_template('settings.mako'), request_method="GET")
+    def settings(self):
+        save_url = self.request.route_url('settings')
+        return TemplateResponseDict(mapping=mapping, save_url=save_url)
