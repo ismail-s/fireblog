0dc3113cb773c9c0ca554f6e7eadc8fa651ae610
Ismail <astroman3d@gmail.com>
2015-11-12 16:27:47 +0000

    Pass mapping to template

--- fireblog/settings/views.py
+++ fireblog/settings/views.py
@@ -6,4 +6,4 @@ from pyramid.view import view_config, view_defaults
 @view_config(route_name='settings', decorator=use_template(
     'settings.mako'), permission=None, request_method="GET")
 def settings(request):
-    return TemplateResponseDict()
+    return TemplateResponseDict(mapping=mapping)
