4faedfae602e35260cdfafa591980e52717910ab
Ismail <astroman3d@gmail.com>
2015-11-12 15:35:57 +0000

    Pep8 conformance

--- fireblog/settings/mapping.py
+++ fireblog/settings/mapping.py
@@ -14,19 +14,23 @@ mapping = (
     Entry(
         'fireblog.max_rss_items',
         'Max number of RSS items',
-        'The maximum number of items to show in the RSS feed. The latest posts are shown in the RSS feed.',
+        'The maximum number of items to show in the RSS feed. '
+        'The latest posts are shown in the RSS feed.',
         int,
         validators.max_rss_item_validator),
     Entry(
         'fireblog.all_view_post_len',
         'Max length of post preview',
-        'Some webpages show previews of several posts. Here, you can set how long those previews can be.',
+        'Some webpages show previews of several posts. '
+        'Here, you can set how long those previews can be.',
         int,
         validators.post_preview_len_validator),
     Entry(
         'persona.siteName',
         'Site name',
-        'Name of the website. If this is changed, then the website should be restarted at some point in order to update the display of the sitename in the login screen.',
+        'Name of the website. If this is changed, then the website should be '
+        'restarted at some point in order to update the display of the '
+        'sitename in the login screen.',
         str,
         validators.sitename_validator)
 )
--- fireblog/settings/views.py
+++ fireblog/settings/views.py
@@ -3,6 +3,7 @@ from fireblog.utils import use_template, TemplateResponseDict
 from pyramid.view import view_config, view_defaults
 
 
-@view_config(route_name='settings', decorator=use_template('settings.mako'), permission=None, request_method="GET")
+@view_config(route_name='settings', decorator=use_template(
+    'settings.mako'), permission=None, request_method="GET")
 def settings(request):
     return TemplateResponseDict()
