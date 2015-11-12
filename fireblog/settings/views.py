ef57e5bc1f1e8dcfca1647075fbb0d49dc2d425a
Ismail <astroman3d@gmail.com>
2015-11-12 14:51:41 +0000

    Add initial settings view

---
+++ fireblog/settings/views.py
@@ -0,0 +1,8 @@
+from fireblog.settings import mapping
+from fireblog.utils import use_template, TemplateResponseDict
+from pyramid.view import view_config, view_defaults
+
+
+@view_config(route_name='settings', decorator=use_template('settings.mako'), permission=None, request_method="GET")
+def settings(request):
+    return TemplateResponseDict()
---
+++ fireblog/templates/bootstrap/settings.mako
@@ -0,0 +1,7 @@
+<%inherit file="navbar.mako"/>
+
+<%block name="header">Settings</%block>
+
+<%block name="content">
+<p>This will be the content...</p>
+</%block>
