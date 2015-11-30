c3c00f49b826d12100d6773a5e85125898fb44d0
Ismail <astroman3d@gmail.com>
2015-11-30 16:59:54 +0000

    Add comments and deal with missing import during test runs

--- fireblog/tasks.py
+++ fireblog/tasks.py
@@ -1,5 +1,12 @@
-from uwsgidecorators import spool
-import uwsgi
+'Tasks that can be run under the uwsgi spooler.'
+# uwsgi python module is added dynamically when the blog is run under uwsgi.
+# As a result, this module won't be available for importing during test runs.
+try:
+    from uwsgidecorators import spool
+    import uwsgi
+except ImportError:
+    uwsgi = {}
+    spool = lambda x: x
 import time
 try:
     from pathlib import Path
@@ -9,6 +16,7 @@ except ImportError:
 
 @spool
 def reload_uwsgi(args):
+    'Waits for a second, then reloads the blog.'
     # Remove all spooler files. This is necessary as this current function
     # doesn't return when it reloads uwsgi, so the spooler file that caused
     # this function to be called won't be deleted, which will result in endless
