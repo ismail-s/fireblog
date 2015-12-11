e8c37c7012b16eeefb2084f70d1d31b3c188c823
Ismail <astroman3d@gmail.com>
2015-12-11 11:37:05 +0000

    Check all settings values all the time

--- fireblog/settings/__init__.py
+++ fireblog/settings/__init__.py
@@ -15,25 +15,24 @@ def make_sure_all_settings_exist_and_are_valid():
     user.
     This function should only be run when the web app is starting up, as it
     requests user input from the command line."""
-    if len(settings_dict) != len(mapping):
-        with transaction.manager:
-            for entry in mapping:
-                try:
-                    value = settings_dict[entry.registry_name]
-                    valid, value, _ = validate_value(entry, value)
-                    if valid:
-                        continue
-                except KeyError:
-                    pass
-                while True:
-                    # Get value from user
-                    input_str = 'Please provide a value for the setting "{}": '
-                    user_val = input(input_str.format(entry.display_name))
-                    valid, value, _ = validate_value(entry, user_val)
-                    if valid:
-                        break
-                    print('That value is invalid.')
-                settings_dict[entry.registry_name] = value
+    with transaction.manager:
+        for entry in mapping:
+            try:
+                value = settings_dict[entry.registry_name]
+                valid, value, _ = validate_value(entry, value)
+                if valid:
+                    continue
+            except KeyError:
+                pass
+            while True:
+                # Get value from user
+                input_str = 'Please provide a value for the setting "{}": '
+                user_val = input(input_str.format(entry.display_name))
+                valid, value, _ = validate_value(entry, user_val)
+                if valid:
+                    break
+                print('That value is invalid.')
+            settings_dict[entry.registry_name] = value
 
 
 def validate_value(entry: Entry, value):
