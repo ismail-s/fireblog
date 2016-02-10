38c9188e16b3a49796bc54a9a04e7fe0e15efebc
Ismail <astroman3d@gmail.com>
2016-02-10 17:43:52 +0000

    Add tests

--- fireblog/tests/settings/db_wrapper_test.py
+++ fireblog/tests/settings/db_wrapper_test.py
@@ -1,21 +1,29 @@
 import pytest
-from fireblog.settings import settings_dict
+from fireblog.settings import settings_dict, mapping
 
 
 pytestmark = pytest.mark.usefixtures("test_with_one_theme")
 
 
-def test_can_set_an_item_and_then_get_the_same_item(pyramid_config):
+def test_can_set_an_item_and_then_get_the_same_item(
+        pyramid_config, clear_settings_dict):
     assert not settings_dict.get('test', None)
     settings_dict['test'] = 'test_str'
     assert settings_dict['test'] == 'test_str'
 
 
-def test_changing_an_item_and_getting_it_returns_new_item(pyramid_config):
+def test_changing_an_item_and_getting_it_returns_new_item(
+        pyramid_config, clear_settings_dict):
     settings_dict['test'] = 'test_str'
     assert settings_dict['test'] == 'test_str'
     settings_dict['test'] = 'test_str1'
     assert settings_dict['test'] == 'test_str1'
+
+
+def test_can_get_default_items(pyramid_config, clear_settings_dict):
+    for entry in mapping:
+        correct_value = entry.type(entry.default_value)
+        assert settings_dict[entry.registry_name] == correct_value
 
 
 def test_can_delete_an_item(pyramid_config):
@@ -37,8 +45,27 @@ def test_can_get_num_of_settings(pyramid_config, clear_settings_dict):
 
 
 def test_can_iterate_over_keys(pyramid_config, clear_settings_dict):
+    results = set()
+    for key in settings_dict:
+        results.add(key)
+    assert results == set([e.registry_name for e in mapping])
+
+
+def test_can_iterate_over_added_keys(pyramid_config, clear_settings_dict):
     settings_dict.update({'1': '1', '2': '2', '3': '3'})
     results = set()
     for key in settings_dict:
         results.add(key)
+    # Get rid of default keys
+    results -= set([e.registry_name for e in mapping])
     assert results == set(['1', '2', '3'])
+
+
+def test_items_are_returned_once_only(pyramid_config, clear_settings_dict):
+    registry_name = mapping[0].registry_name
+    settings_dict[registry_name] = 'testing'
+    counter = 0
+    for key in settings_dict:
+        if key == registry_name:
+            counter += 1
+    assert counter == 1
